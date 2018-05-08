df9cdf762093db6cb7ed7a20514012a93b7d76c1
Dmitry Marov <mds3d14@gmail.com>
2018-05-08 20:28:01 +0000

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,18 @@ const sourcemaps = require('gulp-sourcemaps');
 const htmlMin = require('gulp-htmlmin');
 const inject = require('gulp-inject');
 const stripHtmlComments = require('gulp-strip-comments');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
-gulp.task('default', ['build-css', 'build-html']);
-
-gulp.task('build-css', _ => {
-
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCss({ compatibility: 'ie7' }))
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('public/css'));
-
-});
+gulp.task('default', [
+    'build-html',
+    'build-css',
+    'build-js',
+]);
 
 gulp.task('build-html', _ => {
 
-    return gulp.src('src/html/**/*.html')
+    return gulp.src([ '!src/html/component/*', 'src/html/**/*.html' ])
         .pipe(inject(gulp.src('src/html/component/**/*.html'), {
             starttag: '<!-- inject:{{path}} -->',
             relative: true,
@@ -35,9 +30,34 @@ gulp.task('build-html', _ => {
 
 });
 
+gulp.task('build-css', _ => {
+
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss({ compatibility: 'ie7' }))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('public/css'));
+
+});
+
+gulp.task('build-js', _ => {
+
+    return gulp.src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['env'],
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('public/js'));
+
+});
+
 gulp.task('watch', _ => {
 
     gulp.watch('src/scss/**/*.scss', ['build-css']);
     gulp.watch('src/html/**/*.html', ['build-html']);
+    gulp.watch('src/js/**/*.js', ['build-js']);
 
 });
---
+++ src/js/layout.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* layout class */
+class Layout {
+
+    constructor() {
+
+        this.message = 'ok';
+
+    }
+
+    alert() {
+
+        alert(this.message);
+
+    }
+
+}
+
+let layout = new Layout();
